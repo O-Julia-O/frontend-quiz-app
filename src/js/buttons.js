5cb595621bb3b709b046296633d711eb000ae081
O-Julia-O <julia.qq.onix@gmail.com>
2025-05-02 14:51:22 +0300

    finished header for questions

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -30,11 +30,40 @@ document.addEventListener('DOMContentLoaded', () => {
       // в header добавить текст "Тема: {topic}"
       header.style.justifyContent = "space-between"; // выровнять по краям
       logo.style.display = "flex"; // показать логотип
-      header.querySelector("img").src = quizzes[0].icon; // показать заголовок
-      header.querySelector("img").style.marginRight = "1rem"; // убрать отступ
+      
+      header.querySelector("img").style.marginRight = "1rem"; 
       header.querySelector("h1").textContent = `${topic}`; // добавить текст в заголовок
       header.querySelector("h1").classList.add("text-preset-4-mobile-medium"); // add style for h1
       logo.style.alignItems = "center"; // выровнять логотип по центру
+
+      switch (topic) {
+        case "HTML":
+          header.querySelector("img").src = quizzes[0].icon; // иконка темы
+          header.querySelector("img").classList.add("html-btn"); // добавить класс для иконки
+          break;
+        case "CSS":
+          header.querySelector("img").src = quizzes[1].icon; // иконка темы
+          header.querySelector("img").classList.add("css-btn");// цвет текста
+          break;
+        case "JavaScript":
+          header.querySelector("img").src = quizzes[2].icon; // иконка темы
+          header.querySelector("img").classList.add("js-btn"); // цвет текста
+          break;
+        case "Accessibility":
+          header.querySelector("img").src = quizzes[3].icon; // иконка темы
+          header.querySelector("img").classList.add("access-btn"); // цвет текста
+          break;
+        default:
+          console.error("Unknown topic:", topic);
+      }
+
+      header.querySelector("img").style.padding = "5px"; 
+      header.querySelector("img").style.borderRadius = "12px";  
+
+
+      /* header.querySelector("img").style.backgroundColor = "red"; */
+
+
 
       // и иконку с темой
 
